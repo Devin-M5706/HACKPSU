37e20817547d798b1bd9b1c5272e964ccea1ad81
MattSteele232 <mbs7159@psu.edu>
2024-10-13 02:03:46 -0400

    ai prompt

--- HACKPSU/popup.js
+++ HACKPSU/popup.js
@@ -1,9 +1,9 @@
 import {getResponse} from 'gemini.js';
+import ai_prompt from 'ai_prompt.js';
 window.addEventListener('DOMContentLoaded', async () => {
 	const article = await extract("https://www.cnn.com/2024/10/12/politics/helene-milton-early-voting-north-carolina-florida-south-carolina/index.html");
 	document.body.innerHTML += article;
 	document.getElementById('scanButton').addEventListener('click', async (ev) => {
-		ai_prompt= "";
 		geminiResponse = getResponse(ai_prompt);
 	})
 });
