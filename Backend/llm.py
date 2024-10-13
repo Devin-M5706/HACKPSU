445b6c2b8515ee0b5a133b0b3b9d4e7a46c8e089
Kurbo1 <mcraftryan@gmail.com>
2024-10-12 20:28:19 -0400

    Working on backend

--- Backend/llm.py
+++ Backend/llm.py
@@ -1,11 +1,11 @@
 import ollama
 import scraper
 
-url = scraper.url
+url = "https://www.cnn.com/2024/10/12/climate/hurricane-milton-helene-florida-homes/index.html"
 
 stream = ollama.chat(
     model ='llama3.2',
-    messages=[{'role': 'user','content': 'summarize the contents of this article'}],
+    messages=[{'role': 'user','content': 'Here is unformatted code for a web page, please find the english contents of the article\n' + scraper.get_pageData(url)}],
     stream = True,
 )
 
