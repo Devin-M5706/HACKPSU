62528067c5e5cad83357d80a3968e32e3b344cbd
MattSteele232 <mbs7159@psu.edu>
2024-10-12 23:45:57 -0400

    idek

--- Backend/llm.py
+++ Backend/llm.py
@@ -5,13 +5,12 @@ url = "https://www.cnn.com/2024/10/12/climate/hurricane-milton-helene-florida-ho
 urla = scraper.get_pageData(url)
 stream = ollama.chat(
     model ='llama3.2',
-    messages=[{'role': 'user','content': 'List the claims made by this article and list each one on a new line and in parenthesis %s' %urla}],
-    stream = True,
+    messages=[{'role': 'user','content': 'List the claims made by this article and list each one on a new line and in parenthesis %s' % urla}],
+    stream=True,
 )
 
 def getClaims():
     claim_words = []
     for chunk in stream:
-        print(chunk['message']['content'],end='', flush=True)
-        claim_words += chunk
+        claim_words.append(chunk['message']['content'])
     return claim_words
